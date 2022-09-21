528d5d52a4c6386433b544a1416f01a8b7308c82
Victor Martins <victor.martins.duarte@gmail.com>
2022-09-21 18:20:03 -0300

    Feat: Req-07 Adds logic for Inventory function to make reports of XML files

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -2,6 +2,7 @@ from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 import csv
 import json
+import xml.etree.ElementTree as ET
 
 
 class Inventory:
@@ -13,15 +14,27 @@ class Inventory:
         with open(path) as file:
             return json.load(file)
 
+    def xml(path):
+        list = []
+        xml = ET.parse(path).getroot()
+        for item in xml:
+            item_dict = {}
+            for attribute in item:
+                item_dict[attribute.tag] = attribute.text
+            list.append(item_dict)
+        return list
+
     @classmethod
     def import_data(cls, file_path, report_type):
-        def verify_extension(path):
+        def check_extension(path):
             if path.endswith('.csv'):
                 return cls.csv(path)
             elif path.endswith('.json'):
                 return cls.json(path)
+            else:
+                return cls.xml(path)
 
         if report_type == "simples":
-            return SimpleReport.generate(verify_extension(file_path))
+            return SimpleReport.generate(check_extension(file_path))
         else:
-            return CompleteReport.generate(verify_extension(file_path))
+            return CompleteReport.generate(check_extension(file_path))
