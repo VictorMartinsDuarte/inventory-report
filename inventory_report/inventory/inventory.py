2904799e3bf45990eab72c2619b046d0f5ae523b
Victor Martins <victor.martins.duarte@gmail.com>
2022-09-21 17:42:11 -0300

    Feat: Req-06 Adds logic for Inventory function to filter by CSV or JSON files and make reports

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,16 +1,27 @@
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 import csv
+import json
 
 
 class Inventory:
-    @staticmethod
-    def import_data(csv_path, report_type):
-        with open(csv_path) as file:
-            csv_file = csv.DictReader(file, delimiter=",", quotechar='"')
-            csv_list = list(csv_file)
-            
-            if report_type == "simples":
-                return SimpleReport.generate(csv_list)
-            else:
-                return CompleteReport.generate(csv_list)
+    def csv(path):
+        with open(path) as file:
+            return list(csv.DictReader(file))
+
+    def json(path):
+        with open(path) as file:
+            return json.load(file)
+
+    @classmethod
+    def import_data(cls, file_path, report_type):
+        def verify_extension(path):
+            if path.endswith('.csv'):
+                return cls.csv(path)
+            elif path.endswith('.json'):
+                return cls.json(path)
+
+        if report_type == "simples":
+            return SimpleReport.generate(verify_extension(file_path))
+        else:
+            return CompleteReport.generate(verify_extension(file_path))
