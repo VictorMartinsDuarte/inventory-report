de4e5632c15decc614cbe63b65be391a75dc9f05
Victor Martins <victor.martins.duarte@gmail.com>
2022-09-21 14:41:20 -0300

    Feat: Req-02 Adds tests for test_relatorio_produto function

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,13 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        4, 'notebook', 'ebytr', '21/09/22', '10', '587496', 'Evitar sol')
+
+    assert product.__repr__() == (
+            f"O produto {product.nome_do_produto}"
+            f" fabricado em {product.data_de_fabricacao}"
+            f" por {product.nome_da_empresa} com validade"
+            f" até {product.data_de_validade}"
+            f" precisa ser armazenado {product.instrucoes_de_armazenamento}.")
