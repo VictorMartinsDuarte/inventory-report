b014679101310136ec9e5d482b5b69102b1db598
Victor Martins <victor.martins.duarte@gmail.com>
2022-09-21 14:25:25 -0300

    Feat: Req-01 Adds tests for test_cria_produto function

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,14 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        4, 'notebook', 'ebytr', '21/09/22', '10', '587496', 'Evitar sol')
+
+    assert product.id == 4
+    assert product.nome_do_produto == 'notebook'
+    assert product.nome_da_empresa == 'ebytr'
+    assert product.data_de_fabricacao == '21/09/22'
+    assert product.data_de_validade == '10'
+    assert product.numero_de_serie == '587496'
+    assert product.instrucoes_de_armazenamento == 'Evitar sol'
